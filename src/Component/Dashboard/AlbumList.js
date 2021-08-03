3dde92d868254dc5f5ccb511467b23d4d023c2e0
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2021-08-03 14:08:08 +0700

    get image dimension

--- src/Component/Dashboard/AlbumList.js
+++ src/Component/Dashboard/AlbumList.js
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useRef } from "react";
 
 function AlbumList({ img, label }) {
+  const imageRef = useRef();
+
+  const getImageDimension = (e) => {
+    const img = imageRef.current.clientWidth;
+    console.log(img);
+  };
+
   return (
     <div class="card-album">
       <div className="album">
-        <img src={img} alt="..." />
+        <img src={img} alt="..." ref={imageRef} onLoad={getImageDimension} />
         <h5>{label}</h5>
         <p>Created by you</p>
       </div>
