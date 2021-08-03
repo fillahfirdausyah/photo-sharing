5c6e53e0eda074e6297781b638c120c3a0755ebb
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2021-08-03 14:07:58 +0700

    add responsive in some breakpoints

--- src/Component/Dashboard/index.js
+++ src/Component/Dashboard/index.js
@@ -33,22 +33,22 @@ function Dashboard() {
         </header>
         <div className="content">
           <div className="row">
-            <div className="col-4">
+            <div className="col-12 col-md-6 col-lg-4">
               <AlbumList img={Travel} label="Travel" />
             </div>
-            <div className="col-4">
+            <div className="col-12 col-md-6 col-lg-4">
               <AlbumList img={Dogs} label="Dogs" />
             </div>
-            <div className="col-4">
+            <div className="col-12 col-md-6 col-lg-4">
               <AlbumList img={Hiking} label="Hiking" />
             </div>
-            <div className="col-4">
+            <div className="col-12 col-md-6 col-lg-4">
               <AlbumList img={Camping} label="Camping" />
             </div>
-            <div className="col-4">
+            <div className="col-12 col-md-6 col-lg-4">
               <AlbumList img={Art} label="Art" />
             </div>
-            <div className="col-4">
+            <div className="col-12 col-md-6 col-lg-4">
               <AlbumList img={Architect} label="Architect" />
             </div>
           </div>
