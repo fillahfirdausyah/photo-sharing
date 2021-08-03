1014bac26155c3a9c36ecba6b1bd292a60831812
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2021-08-03 13:03:59 +0700

    make DashboardPage component

---
+++ src/Pages/DashboardPage/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+// Component
+import Dashboard from "../../Component/Dashboard";
+
+function DashboardPage() {
+  return (
+    <>
+      <Dashboard />
+    </>
+  );
+}
+
+export default DashboardPage;
