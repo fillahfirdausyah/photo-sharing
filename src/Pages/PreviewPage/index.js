613e67dd27b6b4406b537be322cb075165ae042f
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2021-08-04 10:08:21 +0700

    make PreviewPage component

---
+++ src/Pages/PreviewPage/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+// Component
+import Header from "../../Component/Header";
+
+function PreviewPage({ title }) {
+  return (
+    <>
+      <Header title={title} />
+    </>
+  );
+}
+
+export default PreviewPage;
