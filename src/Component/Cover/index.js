5cd54ec39786a9fb7429d25e3817d99956b7637f
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2021-08-04 10:09:32 +0700

    make Cover component

---
+++ src/Component/Cover/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+// Asset
+import "./style.css";
+
+function Cover() {
+  return (
+    <div className="cover">
+      <div className="cover-img">
+          <img src="" alt="" />
+      </div>
+    </div>
+  );
+}
+
+export default Cover;
