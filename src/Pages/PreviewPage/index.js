e22c7fa09ff76831e0aca67951a6040102876390
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2021-08-04 10:37:18 +0700

    add and render Cover component

--- src/Pages/PreviewPage/index.js
+++ src/Pages/PreviewPage/index.js
@@ -2,11 +2,13 @@ import React from "react";
 
 // Component
 import Header from "../../Component/Header";
+import Cover from "../../Component/Cover";
 
 function PreviewPage({ title }) {
   return (
     <>
       <Header title={title} />
+      <Cover />
     </>
   );
 }
