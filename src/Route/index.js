8553b702a5083aa76cd6bfa6df3eede5d50cbe7d
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2021-08-04 10:04:08 +0700

    add '/preview route'

--- src/Route/index.js
+++ src/Route/index.js
@@ -1,13 +1,18 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import { DashboardPage } from "../Pages";
+import { DashboardPage, PreviewPage } from "../Pages";
 
 function index() {
   return (
     <div>
       <BrowserRouter>
         <Switch>
-          <Route path="/dashboard" component={DashboardPage} exact />
+          <Route path="/dashboard" exact>
+            <DashboardPage title="Albums" />
+          </Route>
+          <Route path="/preview">
+            <PreviewPage title="Shared Album" /> 
+          </Route>  
         </Switch>
       </BrowserRouter>
     </div>
