054391cfef17bc6bec2eabdb091455c11d0a2dd9
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2021-08-04 10:13:28 +0700

    make exporter file

---
+++ src/Assets/index.js
@@ -0,0 +1,9 @@
+import Architect from "./architect.svg";
+import Art from "./art.svga";
+import Camping from "./camping.svg";
+import Dogs from "./dogs.svg";
+import Hiking from "./hiking.svg";
+import Travel from "./travel.svg";
+import Avatar from "./avatar9.png";
+
+export { Architect, Art, Avatar, Camping, Dogs, Hiking, Travel };
